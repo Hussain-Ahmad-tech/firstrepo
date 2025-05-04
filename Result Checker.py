6671f6ab5bd378ebc765de220751988a667c4b82
Hussain-Ahmad-tech <hussaingenius13@gmail.com>
2025-05-04 19:39:19 +0500

    Add files via upload

---
+++ Result Checker.py
@@ -0,0 +1,10 @@
+Ali = 50
+
+if Ali >= 90:
+    print("Grade A")
+elif Ali >= 80:
+    print("Grade B")
+elif Ali >= 70:
+    print("Grade C")
+else:
+    print("You Failed")
---
+++ While If.py
@@ -0,0 +1,4 @@
+hi = 2
+while hi <= 10:
+    print(hi)
+    hi += 2
